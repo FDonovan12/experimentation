4d4884eddc0f3f1f3c831488d3a1953c687db6ef
donovan <donovanferreira12@gmail.com>
2024-06-30 19:27:51 +0200

    input range game of life here but not functional

--- game_of_life/script.js
+++ game_of_life/script.js
@@ -1,37 +1,68 @@
+const sizePixel = 10;
+let list = [[]];
+
 window.addEventListener('load', () => {
+    const canvas = document.getElementById('canvas');
+    let ctx = canvas.getContext('2d');
+    canvas.addEventListener('click', (event) => {
+        changeSquare(event);
+    });
+    const widthRange = document.getElementById('width');
+    const heightRange = document.getElementById('height');
+    getInputValue(widthRange);
+    widthRange.addEventListener('input', () => {
+        getInputValue(widthRange);
+    });
+    getInputValue(heightRange);
+    heightRange.addEventListener('input', () => {
+        getInputValue(heightRange);
+    });
     initGameOfLife();
 });
-
-window.addEventListener('click', (event) => {
-    changeSquare(event, list);
-});
-// Définition des paramètres
-const sizePixel = 10;
-let list = null;
 
 function printMousePos(event) {
     return 'clientX: ' + event.clientX + ' - clientY: ' + event.clientY;
 }
 
+function getInputValue(input) {
+    console.log('getInputValue :');
+    const value = input.value;
+    const aria = input.ariaLabel;
+    const label = document.getElementById(aria);
+    label.textContent = value;
+    setCanvasSize();
+    printGameOfLife();
+}
+
+function setCanvasSize() {
+    const canvas = document.getElementById('canvas');
+
+    const widthValue = document.getElementById('width').value;
+    canvas.width = widthValue * sizePixel + 1;
+
+    const heightValue = document.getElementById('height').value;
+    canvas.height = heightValue * sizePixel + 1;
+    ctx = canvas.getContext('2d');
+}
+
 function initGameOfLife() {
     const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
-    const width = Math.trunc(canvas.width / sizePixel) - 1;
-    const height = Math.trunc(canvas.height / sizePixel) - 1;
+    setCanvasSize();
+    ctx = canvas.getContext('2d');
+    const width = Math.trunc(canvas.width / sizePixel);
+    const height = Math.trunc(canvas.height / sizePixel);
     ctx.fillRect(0, 0, width * sizePixel + 1, height * sizePixel + 1);
-    list = Array(6).map((e) => Array(5).map((e) => 0));
     list = Array.apply(null, Array(width)).map((e) =>
         Array.apply(null, Array(height)).map((e) => 0)
     );
     list[4][4] = 1;
     list[4][5] = 1;
     list[4][6] = 1;
-    printGameOfLife(ctx, list, sizePixel);
+    printGameOfLife();
 }
 
-function changeSquare(event, list) {
+function changeSquare(event) {
     const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
     const rect = canvas.getBoundingClientRect();
     const mouseX = event.clientX - rect.left;
     const mouseY = event.clientY - rect.top;
@@ -39,24 +70,22 @@ function changeSquare(event, list) {
     const height = Math.trunc(mouseY / sizePixel);
     if (width < list.length && height < list[0].length) {
         list[width][height] = 1 - list[width][height];
-        printGameOfLife(ctx, list, sizePixel);
-    } else {
-        runGameOfLife(ctx, 150, 100, sizePixel);
+        printGameOfLife();
     }
 }
 
-function runGameOfLife(ctx, delay, numberLoop, sizePixel) {
+function runGameOfLife(delay, numberLoop) {
     for (let i = 0; i < numberLoop; i++) {
         setTimeout(() => {
-            list = list.map((row, x) => row.map((square, y) => realoadSquare(list, x, y)));
-            printGameOfLife(ctx, list, sizePixel);
+            list = list.map((row, x) => row.map((square, y) => realoadSquare(x, y)));
+            printGameOfLife();
         }, delay * i);
     }
 }
 
-function realoadSquare(list, x, y) {
+function realoadSquare(x, y) {
     square = list[x][y];
-    count = countAroundSquare(list, x, y);
+    count = countAroundSquare(x, y);
     if (square && [2, 3].includes(count)) {
         return 1;
     }
@@ -66,7 +95,7 @@ function realoadSquare(list, x, y) {
     return 0;
 }
 
-function countAroundSquare(list, x, y) {
+function countAroundSquare(x, y) {
     sum = -list[x][y];
     for (let i = x - 1; i <= x + 1; i++) {
         for (let j = y - 1; j <= y + 1; j++) {
@@ -78,7 +107,7 @@ function countAroundSquare(list, x, y) {
     return sum;
 }
 
-function printGameOfLife(ctx, list, sizePixel) {
+function printGameOfLife() {
     list.forEach((row, x) => {
         row.forEach((square, y) => {
             if (square) {
@@ -86,8 +115,6 @@ function printGameOfLife(ctx, list, sizePixel) {
             } else {
                 ctx.fillStyle = `rgb(255, 255, 255)`;
             }
-            // console.log(x * sizePixel, y * sizePixel, sizePixel, 1);
-
             ctx.fillRect(x * sizePixel + 1, y * sizePixel + 1, sizePixel - 1, sizePixel - 1);
         });
     });
